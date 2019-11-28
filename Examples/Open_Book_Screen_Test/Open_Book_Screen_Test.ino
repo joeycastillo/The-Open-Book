d036b2c5935043ac8e0233df614944dc1a5a0c68
Joey Castillo <jose.castillo@gmail.com>
2019-11-27 20:36:39 -0500

    update screen test to use open book arduino lib

--- Examples/Open_Book_Screen_Test/Open_Book_Screen_Test.ino
+++ Examples/Open_Book_Screen_Test/Open_Book_Screen_Test.ino
@@ -1,22 +1,17 @@
-#include <GxEPD2_BW.h>
+#include <OpenBook.h>
 #include "bitmap.h"
 
-#define MAX_DISPAY_BUFFER_SIZE 15000ul
-#define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPAY_BUFFER_SIZE / (EPD::WIDTH / 8))
-GxEPD2_BW<GxEPD2_420, MAX_HEIGHT(GxEPD2_420)> display(GxEPD2_420(/*CS=77*/ 44, /*DC=*/ 45, /*RST=*/ 46, /*BUSY=*/ 47));
+OpenBook *book;
 
 void setup()
 {
   Serial.begin(115200);
-  delay(100);
-  display.init(115200);
-  display.setFullWindow();
-  display.firstPage();
-  display.fillScreen(GxEPD_WHITE);
-  display.drawInvertedBitmap(0, 0, OpenBookSplash, display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-  while (display.nextPage());
-  delay(2000);
-  display.powerOff();
+  book = new OpenBook();
+  book->configureScreen();
+  delay(1000);
+  Adafruit_EPD *display = book->getDisplay();
+  display->drawBitmap(0, 0, OpenBookSplash, 400, 300, EPD_BLACK);
+  display->display();
 }
 
 void loop()
