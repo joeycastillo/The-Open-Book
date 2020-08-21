a9a060422d38013da9e90b7d670b065d71aa8d8d
Joey Castillo <jose.castillo@gmail.com>
2020-08-21 16:13:40 -0400

    partial and quick modes for IL91874

--- src/OpenBook_IL91874.h
+++ src/OpenBook_IL91874.h
@@ -20,30 +20,10 @@
 #include "Adafruit_EPD.h"
 #include <Arduino.h>
 
-#define IL91874_PANEL_SETTING 0x00
-#define IL91874_POWER_SETTING 0x01
-#define IL91874_POWER_OFF 0x02
-#define IL91874_POWER_OFF_SEQUENCE 0x03
-#define IL91874_POWER_ON 0x04
-#define IL91874_POWER_ON_MEASURE 0x05
-#define IL91874_BOOSTER_SOFT_START 0x06
-#define IL91874_DEEP_SLEEP 0x07
-#define IL91874_DTM1 0x10
-#define IL91874_DATA_STOP 0x11
-#define IL91874_DISPLAY_REFRESH 0x12
 #define IL91874_DTM2 0x13
-#define IL91874_PDTM1 0x14
-#define IL91874_PDTM2 0x15
-#define IL91874_PDRF 0x16
-#define IL91874_LUT1 0x20
-#define IL91874_LUTWW 0x21
-#define IL91874_LUTBW 0x22
-#define IL91874_LUTWB 0x23
-#define IL91874_LUTBB 0x24
-#define IL91874_PLL 0x30
-#define IL91874_CDI 0x50
-#define IL91874_RESOLUTION 0x61
-#define IL91874_VCM_DC_SETTING 0x82
+#define IL91874_PARTIAL_RES 0x90
+#define IL91874_PARTIALIN 0x91
+#define IL91874_PARTIALOUT 0x92
 
 // TODO: don't duplicate this for every screen, move to a common header for EPD drivers.
 typedef enum OpenBookDisplayMode {
@@ -73,6 +53,7 @@ public:
     void powerDown();
     void setDisplayMode(OpenBookDisplayMode displayMode);
     void drawPixel(int16_t x, int16_t y, uint16_t color);
+    void displayPartial(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
 protected:
     void init(OpenBookDisplayMode displayMode);
     uint8_t writeRAMCommand(uint8_t index);
@@ -81,6 +62,16 @@ protected:
 
     OpenBookDisplayMode currentDisplayMode = OPEN_BOOK_DISPLAY_MODE_DEFAULT;
 private:
+    static const unsigned char LUT_VCOM_QUICK[];
+    static const unsigned char LUT_W_QUICK[];
+    static const unsigned char LUT_B_QUICK[];
+
+    static const unsigned char LUT_VCOM_PARTIAL[];
+    static const unsigned char LUT_WW_PARTIAL[];
+    static const unsigned char LUT_WB_PARTIAL[];
+    static const unsigned char LUT_BW_PARTIAL[];
+    static const unsigned char LUT_BB_PARTIAL[];
+
     static const unsigned char LUT_VCOM_GRAYSCALE[];
     static const unsigned char LUT_WW_GRAYSCALE[];
     static const unsigned char LUT_WB_GRAYSCALE[];
